ad99d3c69a5b7ce92c8d2a3dfe88453fdd8ca771
AbdulH <haritani2002@gmail.com>
2022-10-11 17:42:32 +0200

    started project and installed some plugins. also mad so prep (navbar)

---
+++ app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<nav-bar/>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  components: { NavBar },
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
