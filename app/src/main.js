39d10fd39a5c2f4de51f52dcf9263d7d00e12662
AbdulH <haritani2002@gmail.com>
2022-10-17 19:16:42 +0200

    added aos animation

--- app/src/main.js
+++ app/src/main.js
@@ -3,10 +3,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faAddressCard, faBriefcase, faBrain, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 library.add(faAddressCard, faBriefcase, faBrain, faGraduationCap)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
+    .use(AOS.init())
     .mount('#app')
